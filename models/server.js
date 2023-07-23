6044c870b049c36e117940dee1376fc79a947f47
Pedro Fernandez <pedro@pandadesigners.com>
2023-07-23 02:00:41 -0500

    Primer Commit

---
+++ models/server.js
@@ -0,0 +1,46 @@
+/*  *Servidor de Express*  */
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+
+const Sockets = require("./sockets");
+
+class Server {
+	constructor() {
+		this.app = express();
+		this.port = process.env.PORT;
+
+		//*http server
+		this.server = http.createServer(this.app);
+
+		//*configuracion socket
+		this.io = socketio(this.server, {
+			/**Configuraciones */
+		});
+	}
+
+	middlewares() {
+		// * desplegar directorio publico
+		this.app.use(express.static(path.resolve(__dirname, "../public")));
+	}
+
+	settingSockets() {
+		new Sockets(this.io);
+	}
+
+	execute() {
+		//*inicializar middlewares
+		this.middlewares();
+
+		//?inicializar Sockets
+		this.settingSockets();
+
+		//*Inicializar Server
+		this.server.listen(this.port, () => {
+			console.log(`Servidor Corriendo en puerto: ${this.port}`);
+		});
+	}
+}
+
+module.exports = Server;
